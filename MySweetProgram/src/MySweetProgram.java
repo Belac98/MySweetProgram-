e1714ae33a7751ad5429157fd9df2ae885d27d3f
James Williams <James@Calebs-MBP.attlocal.net>
2020-03-04 14:46:36 -0800

    Initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+	 // TODO Auto-generated method stub
+		
+		//int a = 4;
+		//double b = 7;
+		//double c =  a + b;	
+		//int a = 21 % 8;
+		//char isotope = 't';
+		//boolean a = false;
+		//boolean b = true;
+		//boolean c = false;
+		//boolean d = false;
+		//boolean b = true;
+		//if (b == false)
+		//String str = "Car";
+		//System.out.println("You are Great");
+		//Scanner sc = new Scanner(System.in);
+		//String light = sc.next(); 
+		
+		//byte a = 126;
+	    //System.out.println(a);
+		//a++;
+		//System.out.println(a);
+		//a++;
+		//System.out.println(a);
+		//a++;
+		//short a = 32767;
+		//System.out.println(a);
+		//a++;
+		//System.out.println(a);
+		//a++;
+		//int a = 2147483647;
+		//System.out.println(a);
+		//a++;
+		//System.out.println(a);
+		//a++;
+		//long c = 922337203685477580L;
+		//long b = 170L;
+		//long sum = (c * b) + 135;
+		//float d = 8f;
+		//float c = 17f;
+	    //float sum = 17f / 8f;
+		//double d = 72097d;
+	    //double e = 15d;
+		//double sum = 72097d / 15d;
+		
+		
+	    //char a = 'G'; 
+
+	    // Integer data type is generally 
+	    // used for numeric values 
+	    //int i = 89; 
+
+	    // use byte and short 
+	    // if memory is a constraint 
+	    //byte b = 4; 
+
+	    // this will give error as number is 
+	    // larger than byte range 
+	    // byte b1 = 7888888955; 
+
+	    //short s = 56; 
+
+	    // this will give error as number is 
+	    // larger than short range 
+	    // short s1 = 87878787878; 
+
+	    // by default fraction value 
+	    // is double in java 
+	    //double d = 4.355453532; 
+
+	    // for float use 'f' as suffix 
+	    //float f = 4.7333434f; 
+
+	    //System.out.println("char: " + a); 
+	    //System.out.println("integer: " + i); 
+	   //System.out.println("byte: " + b); 
+	    //System.out.println("short: " + s); 
+	    //System.out.println("float: " + f); 
+	    //System.out.println("double: " + d); 
+
+		
+		
+	}
+	}
+
