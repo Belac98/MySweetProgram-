05a17c9326f16e7d6fe03f8571ae94079e0339cc
Belac98 <61770489+Belac98@users.noreply.github.com>
2020-03-10 17:39:54 -0700

    Update Lemonhead.java

--- MySweetProgram/src/Lemonhead.java
+++ MySweetProgram/src/Lemonhead.java
@@ -3,7 +3,11 @@ public class Lemonhead {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+emailapp em1 = new emailapp("John","Smith");
+		
+		//em1.setAlternateEmail("js@gmail.com");
+		//System.out.println(em1.getAlternateEmail());
+		System.out.println(em1.showInfo()); 
 	}
 
 }
